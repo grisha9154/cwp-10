a29e176d15cb7251ec30dc1c1ac0037222449aac
grisha9154 <grisha9154@gmail.com>
2017-11-08 16:43:30 +0300

    Read Handl

--- index.js
+++ index.js
@@ -6,10 +6,24 @@ app.get('/',(req,res)=>{
    res.send('Hello world!!!');
 });
 app.get('/api/films/readall',readAllFilms);
+app.get('/api/films/read',readFilms);
+
 app.listen(3000,()=>{
     console.log("Server listening on port 3000")
 });
 
+function readFilms(req,res) {
+    fs.readFile("./top250.json",(err,copy)=>{
+        let text = JSON.parse(copy);
+        let params = req.query;
+        for(let i=0;i<text.length;i++){
+            if(text[i].id==params.id){
+                res.send(text[i]);
+                break;
+            }
+        }
+    })
+}
 function readAllFilms(req, res) {
     fs.readFile("./top250.json",function (err, copy) {
         let text = JSON.parse(copy);
@@ -18,6 +32,7 @@ function readAllFilms(req, res) {
         res.setHeader('Content-Type','application/json');
         res.end(JSON.stringify(text));
 })}
+
 function sIncPosition(i, ii) {
     if(i.position>ii.position) return 1;
     else if(i.position<ii.position) return -1;
